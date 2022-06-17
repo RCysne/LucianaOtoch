723247e181ad93f3623c2e69858b68d9a4aa56d3
Ronaldo Cysne <cysne@localhost.localdomain>
2022-06-16 23:22:00 -0300

    Carrosel de fotos

--- script.js
+++ script.js
@@ -63,7 +63,9 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+    const slides = button
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]");
 
     const activeSlide = slides.querySelector('[data-active]');
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
@@ -71,7 +73,7 @@ buttons.forEach(button => {
     if(newIndex < 0) newIndex = slides.children.length - 1;
     if(newIndex >= slides.children.length) newIndex = 0
 
-    slides.children.children[newIndex].dataset.active = true;
+    slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active
   })
 });
